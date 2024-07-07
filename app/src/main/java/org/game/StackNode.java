0985ad723970b64b1e6f4774b3459e39a863e500
Bella <annabel.jakob@hotmail.de>
2024-07-07 14:47:38 +0100

    Add custom stack implementation

--- app/src/main/java/org/game/StackNode.java
+++ app/src/main/java/org/game/StackNode.java
@@ -1,2 +1,38 @@
-package org.game;public class StackNode {
+package org.game;
+
+public class StackNode {
+    private final Card card;
+    private StackNode nextNode;
+
+    public StackNode(Card card) {
+        this.card = card;
+    }
+
+    public StackNode(Card card, StackNode nextNode) {
+        this.card = card;
+        this.nextNode = nextNode;
+    }
+
+    public void setNextNode(StackNode nextNode) {
+        this.nextNode = nextNode;
+    }
+
+    public boolean hasNext() {
+        return (this.nextNode != null);
+    }
+
+    public StackNode unsetNextNode() {
+        StackNode removedNode = this.nextNode;
+        this.nextNode = null;
+
+        return removedNode;
+    }
+
+    public StackNode getNextNode() {
+        return this.nextNode;
+    }
+
+    public Card getCard() {
+        return this.card;
+    }
 }
--- app/src/test/java/org/game/StackNodeTest.java
+++ app/src/test/java/org/game/StackNodeTest.java
@@ -2,9 +2,28 @@ package org.game;
 
 import org.junit.jupiter.api.Test;
 
-public class StackNode {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+public class StackNodeTest {
     @Test
-    void testUnsetNextNode_returnsCorrectNode() {
-        
+    void testUnsetNextNode_whenNextNodeExists_returnsCorrectNode() {
+        Card card = new Card(Suits.HEART, Ranks.TEN);
+        StackNode nextNode = new StackNode(card);
+        StackNode currentNode = new StackNode(card, nextNode);
+
+        StackNode actualNextNode = currentNode.unsetNextNode();
+
+        assertEquals(nextNode, actualNextNode);
+    }
+
+    @Test
+    void testUnsetNextNode_whenNextNodeDoesNotExist_returnsCorrectNode() {
+        Card card = new Card(Suits.HEART, Ranks.TEN);
+        StackNode currentNode = new StackNode(card);
+
+        StackNode actualNextNode = currentNode.unsetNextNode();
+
+        assertNull(actualNextNode);
     }
 }
